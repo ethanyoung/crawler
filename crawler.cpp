75c2e4a8e1db04d4cb9638d833b4d1553ac30534
Ethan Yang <ethan.y@outlook.com>
2015-07-10 05:57:57 -0700

    First commit

---
+++ crawler.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+#include <list>
+#include <queue>
+
+using namespace std;
+
+int main()
+{
+  string url = "www.google.com";
+  cout << url;
+  return 0;
+}
+
