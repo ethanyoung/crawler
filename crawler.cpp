e51456b74ce0457f29a11f4da73bafe5082cc24d
Ethan Yang <ethan.y@outlook.com>
2015-07-11 07:01:27 -0700

    temp

--- crawler.cpp
+++ crawler.cpp
@@ -4,14 +4,17 @@
 #include <list>
 #include <queue>
 #include <set>
+
 #include <curl/curl.h>
+#include <htmlcxx/html/ParserDom.h>
 
 using namespace std;
+using namespace htmlcxx;
 
+/*
 class WebPage {
   list<string> urls;
   string body;
-
   string[] getUniqWords() {
     set<string> uniqueWords;
     // string[] words = body.split(" ");
@@ -24,13 +27,27 @@ class WebPage {
     return getUniqWords().size();
   }
 }
+*/
 
-string getPageContent(string url) {
+string getPageContent() {
   stirng content = "";
-  return content;
-}
+  CURL *curl;
+  FILE *fp;
+  CURLcode res;
+  char *url = "https://www.messagexchange.com";
+  char outfilename[FILENAME_MAX] = "content.txt";
+  curl = curl_easy_init();
+  if (curl) {
+      fp = fopen(outfilename,"wb");
+      curl_easy_setopt(curl, CURLOPT_URL, url);
+      curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_data);
+      curl_easy_setopt(curl, CURLOPT_WRITEDATA, fp);
+      res = curl_easy_perform(curl);
 
-Vextor<string> getLinks(string content) {
+      curl_easy_cleanup(curl);
+      fclose(fp);
+  }
+  return content;
 }
 
 size_t write_data(void *ptr, size_t size, size_t nmemb, FILE *stream) {
@@ -39,21 +56,20 @@ size_t write_data(void *ptr, size_t size, size_t nmemb, FILE *stream) {
 }
 
 int main(void) {
-    CURL *curl;
-    FILE *fp;
-    CURLcode res;
-    char *url = "http://www.google.com";
-    char outfilename[FILENAME_MAX] = "content.txt";
-    curl = curl_easy_init();
-    if (curl) {
-        fp = fopen(outfilename,"wb");
-        curl_easy_setopt(curl, CURLOPT_URL, url);
-        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_data);
-        curl_easy_setopt(curl, CURLOPT_WRITEDATA, fp);
-        res = curl_easy_perform(curl);
-
-        curl_easy_cleanup(curl);
-        fclose(fp);
-    }
-    return 0;
+  int n = 10;
+  string html = getPageContent();
+  //getLinks(html);
+  // if queue.size() < 10
+  // for each links
+  // visit 
+  // get body
+  // body.split.getUniqWords.size()
+  //
+  // for each links
+  // visit
+  // get body
+  // body.split getUniqWords
+  // total.insert uniqueWords
+  // total.size()
+  return 0;
 }
