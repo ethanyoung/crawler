abe054a5e2bf5d076fcd267fbf913d28e7bd3d9a
Ethan Yang <ethan.y@outlook.com>
2015-07-11 04:25:37 -0700

    Dump all the links in the tree

--- parser.cpp
+++ parser.cpp
@@ -10,5 +10,15 @@ int main()
   tree<HTML::Node> dom = parser.parseTree(html);
 
   cout << dom << endl;
+
+  tree<HTML::Node>::iterator it = dom.begin();
+  tree<HTML::Node>::iterator end = dom.end();
+  for (; it != end; ++it) {
+    if (it -> tagName() == "a") {
+      it -> parseAttributes();
+      cout << it -> attribute("href").second;
+    }
+  }
+
   return 0;
 }
