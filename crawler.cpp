fa3da17c3aed08d8318c8ce2a46ae53e2b4a5f4e
Ethan Yang <ethan.y@outlook.com>
2015-07-12 02:13:25 -0700

    Only retrieve html not body

--- crawler.cpp
+++ crawler.cpp
@@ -11,21 +11,27 @@
 using namespace std;
 using namespace htmlcxx;
 
+/*
 size_t write_data(void *ptr, size_t size, size_t nmemb, FILE *stream) {
     size_t written = fwrite(ptr, size, nmemb, stream);
     return written;
 }
+*/
 
 size_t write_to_string(void *ptr, size_t size, size_t count, void *stream) {
   ((string*)stream)->append((char*)ptr, 0, size*count);
   return size*count;
 }
 
-/*
-class WebPage {
-  list<string> urls;
+
+class Page {
+public: 
   string body;
-  string[] getUniqWords() {
+  Page (string b) {
+    body = b;
+  }
+  /*
+  set<string> getUniqWords() {
     set<string> uniqueWords;
     // string[] words = body.split(" ");
     for(int i = 0; i < words.length; i++) {
@@ -36,20 +42,19 @@ class WebPage {
   int countUniqWords() {
     return getUniqWords().size();
   }
-}
-*/
+  */
+};
 
-string getPageContent() {
+string getPageContent(string url) {
   string content = "";
   CURL *curl;
   //FILE *fp;
   CURLcode res;
-  char *url = "https://www.messagexchange.com";
   //char outfilename[FILENAME_MAX] = "content.txt";
   curl = curl_easy_init();
   if (curl) {
       //fp = fopen(outfilename,"wb");
-      curl_easy_setopt(curl, CURLOPT_URL, url);
+      curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
       curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_to_string);
       curl_easy_setopt(curl, CURLOPT_WRITEDATA, &content);
       res = curl_easy_perform(curl);
@@ -60,8 +65,8 @@ string getPageContent() {
   return content;
 }
 
-vector<string> getLinks(string html, int depth) {
-  vector<string> links;
+set<string> getLinks(string html) {
+  set<string> links;
 
   HTML::ParserDom parser;
   tree<HTML::Node> dom = parser.parseTree(html);
@@ -73,19 +78,70 @@ vector<string> getLinks(string html, int depth) {
     if (it->tagName() == "a") {
       it->parseAttributes();
       string link = it->attribute("href").second;
-      cout << link << endl;
-      links.push_back(link);
+      links.insert(link);
     }
   }
 
   return links;
 }
+/*
+string getBody(string html) {
+  string body;
+  HTML::ParserDom parser;
+  tree<HTML::Node> dom = parser.parseTree(html);
+
+  tree<HTML::Node>::iterator it = dom.begin();
+  tree<HTML::Node>::iterator end = dom.end();
+
+  for (; it != end; ++it) {
+    if (it->tagName() == "body") {
+      
+      cout << it->text() << endl;
+      body = it->text();
+      break;
+    }
+  }
+  return body;
+}
+*/
+list<Page> crawl(string startUrl, int depth) {
+  list<Page> pages;
+  queue<string> unvisited;
+  unvisited.push(startUrl);
+
+  while (!unvisited.empty()) {
+    string link = unvisited.front();
+    unvisited.pop();
+    string html = getPageContent(link);
+    if (unvisited.size() < depth){
+      set<string> links = getLinks(html);
+      set<string>::iterator it = links.begin();
+      set<string>::iterator end = links.end();
+      for (; it != end; ++it) {
+        unvisited.push(*it);
+      }
+    }
+    string body = html;
+    Page *page = new Page(body);
+    pages.push_back(*page);
+    if(pages.size() >= depth) {
+      break;
+    }
+
+  }
+  return pages;
+}
 
 int main(void) {
   int n = 10;
-  string html = getPageContent();
-  //cout << html << endl;
-  getLinks(html, n);
+  string url = "https://www.messagexchange.com";
+  list<Page> pages = crawl(url, n);
+  list<Page>::iterator it = pages.begin();
+  list<Page>::iterator end = pages.end();
+  for (; it != end; ++it) {
+    cout << it->body <<endl;
+  }
+  
   // if queue.size() < 10
   // for each links
   // visit 
